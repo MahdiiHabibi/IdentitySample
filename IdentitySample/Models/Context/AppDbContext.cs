bf77ebee7dbdaed94a38257a7d1029c82a1dca9f
Mahdi <mahdihabibi813@gmail.com>
2024-08-04 22:19:19 +0330

    Add User
    Add Users Register Dynamic And With Google
    Log in

---
+++ IdentitySample/Models/Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentitySample.Models.Context
+{
+    public class AppDbContext : IdentityDbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+        public AppDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // call the base if you are using Identity service.
+            // Important
+            base.OnModelCreating(builder);
+
+            // Code here ...
+        }
+    }
+}
